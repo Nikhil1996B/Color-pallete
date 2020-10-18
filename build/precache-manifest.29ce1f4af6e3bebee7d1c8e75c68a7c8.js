21ef24cada26e52872132f050d5f12abc76d5975
Nikhil1996B <b.nikhil.nikhil@gmail.com>
2020-10-18 18:14:25 +0530

    Adding build packages

---
+++ build/precache-manifest.29ce1f4af6e3bebee7d1c8e75c68a7c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5645ed87f788699af0e664d1fb622800",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b325c430241c6f18e2c7",
+    "url": "/static/css/main.704df381.chunk.css"
+  },
+  {
+    "revision": "23bfcc0421374675ae89",
+    "url": "/static/js/2.b6527ec4.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.b6527ec4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b325c430241c6f18e2c7",
+    "url": "/static/js/main.a7f78f69.chunk.js"
+  },
+  {
+    "revision": "5387050906b8ed53cb42",
+    "url": "/static/js/runtime-main.ec2d9410.js"
+  }
+]);
